f8ced08a72ec020582d250e663869d4a93b4f2b2
Hilario <165733390+Hilariorosi54@users.noreply.github.com>
2026-03-21 15:02:51 -0300

    Update entorno.sh
    se  informo que pasa en cada linea

--- entorno.sh
+++ entorno.sh
@@ -9,3 +9,13 @@ crear_entorno() {
         echo "Entorno creado correctamente en $Base"
     fi
 }
+
+
+
+#variable con la ruta donde se crea
+#define la funcion crear_entorno
+#muestra error si ya existe
+#crea las carpetas
+#se confirma que se creo bien
+#se cierra la estructura condicional 
+#cierra la funcion
